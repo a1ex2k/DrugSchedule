ceaf92f2c151105496b84de3eac855928b682f81
a1ex2k <aleksey7ananko@gmail.com>
2024-02-07 23:28:51 +0300

    Adding util projects
    Former-commit-id: 3352d696fd818a42f70168c482acb2dfcc82f714
    Former-commit-id: bb5ebb38130a7062328864d34e026590f0a1409b

---
+++ Utils/DrugScheduleFill/FileInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DrugScheduleFill;
+
+public class FileInfo
+{
+    public required Guid Guid { get; set; }
+
+    public required string OriginalName { get; set; }
+
+    public required string Extension { get; set; }
+
+    public required FileCategory FileCategory { get; set; }
+
+    public required string MediaType { get; set; }
+
+    public required long Size { get; set; }
+
+    public required DateTime CreatedAt { get; set; }
+}
---
+++ Utils/DrugScheduleFill/Medicament.cs
@@ -0,0 +1,17 @@
+﻿namespace DrugScheduleFill;
+
+public class Medicament
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public string? Description { get; set; }
+    
+    public string? Composition { get; set; }
+    
+    public required int ReleaseFormId { get; set; }
+
+    public required int? ManufacturerId { get; set; }
+
+}
