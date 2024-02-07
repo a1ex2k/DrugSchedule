9fd1c593874ad2e86684af3b9e12c2a827ce3adb
a1ex2k <aleksey7ananko@gmail.com>
2024-02-07 23:28:51 +0300

    Adding util projects
    Former-commit-id: fea85208fd827dd10a4e9c47c0e4f5a7c7de2ee6 [formerly 3352d696fd818a42f70168c482acb2dfcc82f714]
    Former-commit-id: bb5ebb38130a7062328864d34e026590f0a1409b
    Former-commit-id: ceaf92f2c151105496b84de3eac855928b682f81

---
+++ Utils/DrugScheduleFill/FileCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace DrugScheduleFill;
+
+public enum FileCategory
+{
+    None = 0,
+    MedicamentImage = 1,
+    MedicamentInstruction = 2,
+    UserAvatar = 3,
+    DrugConfirmation = 4,
+    UserMedicamentImage = 5,
+}
