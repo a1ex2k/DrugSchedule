3352d696fd818a42f70168c482acb2dfcc82f714
a1ex2k <aleksey7ananko@gmail.com>
2024-02-07 23:28:51 +0300

    Adding util projects

---
+++ Utils/DrugScheduleFill/MedicamentFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrugScheduleFill;
+
+public class MedicamentFile
+{
+    public long Id { get; set; }
+
+    public int MedicamentId { get; set; }
+
+    public required Guid FileGuid { get; set; }
+}
