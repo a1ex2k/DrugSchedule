4ab9078b2535912e89ccba10299d24db869d40d5
a1ex2k <aleksey7ananko@gmail.com>
2024-02-14 23:28:28 +0300

    14/02v2
    Former-commit-id: 366e3638ea903cde289f597a5a4e0739398357df
    Former-commit-id: d830aa512e38d9079874d49d56aebe0395dff47c

---
+++ DrugSchedule.StorageContract/Data/Schedule/Special/ScheduleShare.cs
@@ -0,0 +1,10 @@
+﻿namespace DrugSchedule.StorageContract.Data;
+
+public class ScheduleShare
+{
+    public long Id { get; set; }
+
+    public UserContactSimple UserContact { get; set; } = default!;
+
+    public string? Comment { get; set; }
+}
---
+++ DrugSchedule.StorageContract/Data/Schedule/Special/TakingScheduleExtended.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DrugSchedule.StorageContract.Data;
+
+public class TakingScheduleExtended
+{
+    public long Id { get; set; }
+
+    public MedicamentSimple? GlobalMedicament { get; set; }
+
+    public UserMedicamentSimple? UserMedicament { get; set; }
+
+    public string? Information { get; set; }
+
+    public required DateTime CreatedAt { get; set; }
+
+    public required bool Enabled { get; set; }
+
+    public List<ScheduleRepeatPlain> ScheduleRepeats { get; set; } = new();
+
+    public List<ScheduleShare> ScheduleShares { get; set; } = new();
+}
