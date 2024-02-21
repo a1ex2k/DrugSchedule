3fcd3732a7d85c9d82a53f4457a3483fb325fcaf
a1ex2k <aleksey7ananko@gmail.com>
2024-02-21 23:09:46 +0300

    21/02 v2
    Former-commit-id: 384b532f672a4d37438564ec03e90ffc9ba20820 [formerly db9b5a8834a3083cbda7340714ba4b8ef79944b8]
    Former-commit-id: 7da4ed65520bf02aebac59465c5746bf9c80b826
    Former-commit-id: 87a380644db82667a4ab4916bd1d5d2bcf658b7c

---
+++ DrugSchedule.Client/Networking/ITokenStorage.cs
@@ -0,0 +1,67 @@
+﻿using Blazored.LocalStorage;
+
+namespace DrugSchedule.Client.Networking;
+
+public interface ITokenStorage
+{
+    ValueTask<bool> IsSetAsync();
+
+    string GetAccessToken();
+
+    string GetRefreshToken();
+
+    ValueTask RemoveTokensAsync();
+
+    ValueTask WriteTokensAsync(string accessToken, string refreshToken);
+}
+
+public class TokenStorage : ITokenStorage
+{
+    private string? _accessToken;
+    private string? _refreshToken;
+
+    private readonly ILocalStorageService _localStorage;
+
+    private const string LocalStorageAccessTokenKey = "access-token";
+    private const string LocalStorageRefreshTokenKey = "refresh-token";
+
+    public TokenStorage(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+
+    private async ValueTask ReadFromLocalStorageIfNotSet()
+    {
+        if (_accessToken == null || _refreshToken == null)
+        {
+            _accessToken = await _localStorage.GetItemAsStringAsync(LocalStorageAccessTokenKey);
+            _refreshToken = await _localStorage.GetItemAsStringAsync(LocalStorageRefreshTokenKey);
+        }
+    }
+    
+    public async ValueTask<bool> IsSetAsync()
+    {
+        await ReadFromLocalStorageIfNotSet();
+        return _accessToken == null || _refreshToken == null;
+    }
+
+    public string GetAccessToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetRefreshToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask RemoveTokensAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask WriteTokensAsync(string accessToken, string refreshToken)
+    {
+        throw new NotImplementedException();
+    }
+}
