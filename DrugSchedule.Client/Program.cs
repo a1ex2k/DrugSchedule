97ef0b2402dc7af39c98bcb389c8921e1cec4001
a1ex2k <aleksey7ananko@gmail.com>
2024-02-22 16:58:21 +0300

    22/02 v1
    Former-commit-id: 57767130f409a09827eb372a172baf755e30c5d4

--- DrugSchedule.Client/Program.cs
+++ DrugSchedule.Client/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Configuration.AddJsonFile("appsettings.json");
 
 builder.Services
     .AddBlazorise(options =>
@@ -17,7 +18,10 @@ builder.Services
     })
     .AddBootstrap5Providers()
     .AddFontAwesomeIcons();
+
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var apiBaseUri = builder.Configuration.GetValue<string>("ApiBaseUri");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUri!) });
 
 await builder.Build().RunAsync();
